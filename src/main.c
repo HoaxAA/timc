740cf91b194b8821a5071ac3ccee6751f0ce60d1
Hoax <149868997+HoaxAA@users.noreply.github.com>
2025-09-25 00:56:05 +0330

    Add two new features
    implement the isInitialized function to indicate whether the "time" file
    is empty or not
    also the initializedTime function will read the time from the "time" file (if it exists)
    
    Signed-off-by:HoaxAA

--- src/main.c
+++ src/main.c
@@ -6,19 +6,22 @@
 #include <string.h>
 
 void initialize(FILE*);
-ssize_t isInitialized(FILE*);
+ssize_t initializedTime(FILE*);
+long int isInitialized(FILE*);
 
 int
 main()
 {
-    FILE* initializedTime = fopen("time", "a+");
+    FILE* filePtr = fopen("time", "r");
 
-    printf("%zu\n",isInitialized(initializedTime));
+    printf("%d\n", fseek(filePtr, 0, SEEK_END));
+    printf("%lu\n", ftell(filePtr));
 
-    fclose(initializedTime);
+
+    fclose(filePtr);
 }
 
-void initialize(FILE* initializedTime)
+void initialize(FILE* filePtr)
 {
     // Set the timer variable value to the current time of calander system (since epoch)
     // with the help of the time() function
@@ -27,11 +30,11 @@ void initialize(FILE* initializedTime)
     // Save the current time calculated with timer value with the help of ctime function
     char* buffer = ctime(&timer);
     // Write (re-write if already exists) to the text file we opened at first of the file
-    fwrite(buffer, sizeof(char), strlen(buffer), initializedTime);
+    fwrite(buffer, sizeof(char), strlen(buffer), filePtr);
     printf("%s", buffer);
 }
 
-ssize_t isInitialized(FILE* filePtr)
+ssize_t initializedTime(FILE* filePtr)
 {
     char* buffer = malloc(sizeof(char) * 256);
     size_t leng = 255;
@@ -40,4 +43,18 @@ ssize_t isInitialized(FILE* filePtr)
     printf("buffer: %s, count: %zu\n", buffer, status);
     free(buffer);
     return status;
+}
+
+long int isInitialized(FILE* filePtr)
+{
+    // fseek sets the reading pointer where we need, in our case 
+    // we want to check wheter the file is empty or not, so we want
+    // to put that pointer at the end of the file (SEEK_END)
+    // and with the help of ftell we read the total number of bytes
+    // from the begging of the file until where our pointer is (end of the file)
+    // if the file is empty so ftell will return 0 otherwise it will return any other number
+    
+    fseek(filePtr, 0, SEEK_END);
+    long int position = ftell(filePtr);
+    return position;
 }
