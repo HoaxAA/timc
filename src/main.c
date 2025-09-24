eea320b6efaaa2f926e93ca1a32ce334668712ec
Hoax <149868997+HoaxAA@users.noreply.github.com>
2025-09-24 21:49:07 +0330

    Initializing for the first time works
    initializing works but getline isn't behaving
    i should find out what is going on ?
    Hold on, I think i got it while i was writing this XD
    
    Signed-off-by:HoaxAA

--- src/main.c
+++ src/main.c
@@ -1,27 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
 #include <malloc.h>
+#include <string.h>
+
+void initialize(FILE*);
+int isInitialized(FILE*);
 
 int
 main()
 {
-    time_t* initialize = malloc(sizeof(int) * 256);
-    time(initialize);
-    //printf("%lu\n", *initialize);
+    FILE* initializedTime = fopen("time", "w+");
+
+    initialize(initializedTime);
+
+    if (!isInitialized(initializedTime)) {
+        printf("Salam");
+    }
+    else {
+        printf("Khodafes");
+    }
+
+    fclose(initializedTime);
+}
+
+void initialize(FILE* initializedTime)
+{
+    // Set the timer variable value to the current time of calander system (since epoch)
+    // with the help of the time() function
+    time_t timer = time(NULL);
+
+    // Save the current time calculated with timer value with the help of ctime function
+    char* buffer = ctime(&timer);
+    // Write (re-write if already exists) to the text file we opened at first of the file
+    fwrite(buffer, sizeof(char), strlen(buffer), initializedTime);
+    printf("%s", buffer);
+}
+
+int isInitialized(FILE* filePtr)
+{
     char* buffer = malloc(sizeof(char) * 256);
-    ctime_r(initialize, buffer);
-    printf("initialized time: %s\n", buffer);
+    size_t leng = 0;
 
-    sleep(3);
-    time_t current = time(NULL);
-    ctime_r(&current, buffer);
-    printf("elapsed time: %s\n", buffer);
-
-    time_t diff = difftime(current, *initialize);
-
-    printf("Total time spent: %lu seconds\n", diff);
-
-    free(buffer);
-    free(initialize);
+    ssize_t status = getline(&buffer, &leng, filePtr);
+    printf("buffer: %s, count: %zu\n", buffer, status);
+    return status;
 }
