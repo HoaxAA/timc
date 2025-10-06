f103954491cc2d54d14bd2b30d47d966cbbc3ee7
Hoax <149868997+HoaxAA@users.noreply.github.com>
2025-10-06 21:54:01 +0330

    feat: counter function
    counter function keeps track of the elapsed time for
    certain task
    
    Signed-off-by:HoaxAA

--- src/main.c
+++ src/main.c
@@ -4,12 +4,11 @@
 #include <time.h>
 
 int task(char*);
+double counter(char*);
 
 int
 main()
 {
-    char name[] = {"salam"};
-    task(name);
 }
 
 int task(char* name)
@@ -21,10 +20,15 @@ int task(char* name)
        0 will be returned, if it was new, then 1 will be returned and if 
        program fail at some point it'll return -1 */
     FILE* fp = fopen(".time", "a+");
+    if (fp == NULL) {
+        perror("Failed to open .time file\n");
+        return -1;
+    }
 
     char* buffer = malloc(sizeof(char) * 256);
     if (buffer == NULL) {
-        perror("Failed to initialize buffer\n");
+        perror("Failed to allocate memory\n");
+        fclose(fp);
         return -1;
     }
 
@@ -36,9 +40,47 @@ int task(char* name)
         }
     }
     if (exists) {
+        fclose(fp);
+        free(buffer);
         return 0;
     }
     fseek(fp, 0, SEEK_END);
     fprintf(fp, "%s:", name);
+    fclose(fp);
+    free(buffer);
     return 1;
+}
+
+double counter(char* name)
+{
+    /* Save the time (since epoch) into .time.bak file with the task name
+       if that task name already exists the current time would be subtracted
+       to the saved time and it will be returned
+       Input: Task name
+       Return: if task didn't exist return value will be 0, if it exist it will be 
+       subtracted value. If in any point of program any errors encounter 1 will returned */
+
+    FILE* fp = fopen(".time.bak", "a+");
+    if (fp == NULL) {
+        perror("Failed to open .time.bak file\n");
+        return 1;
+    }
+
+    char* buffer = malloc(sizeof(char) * 256);
+    if (buffer == NULL) {
+        perror("Failed to allocate memory");
+        fclose(fp);
+        return 1;
+    }
+
+    time_t now = time(NULL);
+
+    while (fgets(buffer, 255, fp)) {
+        if (buffer == name) {
+
+        }
+    }
+    fseek(fp, 0, SEEK_END);
+    fprintf(fp, "%s:%ld:", name, now);
+    return 0;
 }
